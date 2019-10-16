dc8cc2ba6ff117f8c78d134ba38c44e4448c247c
Huda Yousif <hjdoski@gmail.com>
2019-10-16 23:33:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -111,7 +111,17 @@ class TicTacToe
   end
     
   def winner 
-    
+    index = []
+    index = won?
+    if index == false
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
   end 
   
 end 
