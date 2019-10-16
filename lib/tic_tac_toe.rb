5bfa764e9ba43f57684cf87ad78b74751cc16895
Huda Yousif <hjdoski@gmail.com>
2019-10-16 23:08:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,5 +19,8 @@ class TicTacToe
     return " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n-----------\n #{@board[6]} | #{@board[7]} | #{@board[8]} "
    end
    
-   def input_to_index
+   def input_to_index(index)
+     
+   end 
+   
 end 
