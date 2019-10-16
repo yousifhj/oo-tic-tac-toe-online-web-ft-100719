93a634d429891bbe8815b27f635dfecf2d2e66b9
Huda Yousif <hjdoski@gmail.com>
2019-10-16 23:34:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -124,5 +124,17 @@ class TicTacToe
     end
   end 
   
+  def play
+    until over? == true
+      turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+  
 end 
 
